14542182dfc7a47518004e8dbab7580ffc081f88
Anderson Santos <anderson.feitoza.santos@gmail.com>
2022-01-02 11:06:54 -0300

    chore: initial commit

---
+++ telefonia/lib/assinante.ex
@@ -0,0 +1,19 @@
+defmodule Assinante do
+  defstruct nome: nil, numero: nil, cpf: nil, plano: nil
+
+  @assinantes %{:prepago => "pre.txt", :pospago => "pos.txt"}
+  def cadastrar(nome, numero, cpf, plano \\ :prepago) do
+    (read(plano) ++ [%__MODULE__{nome: nome, numero: numero, cpf: cpf, plano: plano}])
+    |> :erlang.term_to_binary()
+    |> write(plano)
+  end
+
+  defp write(lista_assinantes, plano) do
+    File.write(@assinantes[plano], lista_assinantes)
+  end
+
+  defp read(plano) do
+    {:ok, assinantes} = File.read(@assinantes[plano])
+    :erlang.binary_to_term(assinantes)
+  end
+end
