9706bd6e94956fe7681cb7d7e0d9162bffe8e1cf
Anderson Santos <anderson.feitoza.santos@gmail.com>
2022-01-02 11:46:42 -0300

    refactor: simplfy functions

--- telefonia/lib/assinante.ex
+++ telefonia/lib/assinante.ex
@@ -3,10 +3,26 @@ defmodule Assinante do
 
   @assinantes %{:prepago => "pre.txt", :pospago => "pos.txt"}
 
-  def buscar_assinante(numero) do
-    (read(:prepago) ++ read(:pospago))
-    |> Enum.find(fn assinante -> assinante.numero == numero end)
+  def buscar_assinante(numero, key \\ :all), do: buscar(numero, key)
+
+  defp buscar(numero, :prepago) do
+    assinantes_prepago()
+    |> Enum.find(&(&1.numero == numero))
   end
+
+  defp buscar(numero, :pospago) do
+    assinantes_pospago()
+    |> Enum.find(&(&1.numero == numero))
+  end
+
+  defp buscar(numero, :all) do
+    assinantes()
+    |> Enum.find(&(&1.numero == numero))
+  end
+
+  def assinantes(), do: read(:prepago) ++ read(:pospago)
+  def assinantes_prepago(), do: read(:prepago)
+  def assinantes_pospago(), do: read(:pospago)
 
   def cadastrar(nome, numero, cpf, plano \\ :prepago) do
     case buscar_assinante(numero) do
@@ -28,6 +44,10 @@ defmodule Assinante do
 
   defp read(plano) do
     {:ok, assinantes} = File.read(@assinantes[plano])
-    :erlang.binary_to_term(assinantes)
+
+    case assinantes do
+      "" -> []
+      binary -> :erlang.binary_to_term(binary)
+    end
   end
 end
